796a6a04ccd3d68d7b92ba8f1e2990e6599744aa
Sekiya Saki <s24G1084BZ@s.chibakoudai.jp>
2024-10-29 10:50:55 +0900

    app5に判定を追加

--- app5.js
+++ app5.js
@@ -37,11 +37,23 @@ app.get("/janken", (req, res) => {
   if( num==1 ) cpu = 'グー';
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
-  // ここに勝敗の判定を入れる
-  // 今はダミーで人間の勝ちにしておく
-  let judgement = '勝ち';
-  win += 1;
-  total += 1;
+
+  let judgement = '';
+  if (hand === cpu) {
+    judgement = 'あいこ'; 
+  } else if (
+    (hand === 'グー' && cpu === 'チョキ') ||
+    (hand === 'チョキ' && cpu === 'パー') ||
+    (hand === 'パー' && cpu === 'グー')
+  ) {
+    judgement = '勝ち'; 
+    win += 1; 
+  } else {
+    judgement = '負け'; 
+  }
+
+  total += 1; 
+
   const display = {
     your: hand,
     cpu: cpu,
