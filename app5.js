35359506a6143e9ff3d194dbc1b20416ee004362
Sekiya Saki <s24G1084BZ@s.chibakoudai.jp>
2024-11-18 13:22:52 +0900

    はじめにを追加

--- app5.js
+++ app5.js
@@ -5,19 +5,6 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/hello1", (req, res) => {
-  const message1 = "Hello world";
-  const message2 = "Bon jour";
-  res.render('show', { greet1:message1, greet2:message2});
-});
-
-app.get("/hello2", (req, res) => {
-  res.render('show', { greet1:"Hello world", greet2:"Bon jour"});
-});
-
-app.get("/icon", (req, res) => {
-  res.render('icon', { filename:"./public/Apple_logo_black.svg", alt:"Apple Logo"});
-});
 
 app.get("/luck", (req, res) => {
   const num = Math.floor( Math.random() * 6 + 1 );
